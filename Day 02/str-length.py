f9cbb87c8037edaf65c1f377ffbb4a35ebdb61ce
MasudDevSecOps <rahmanmdm16@gmail.com>
2023-10-29 19:55:18 -0500

    New commit

--- Day 02/str-length.py
+++ Day 02/str-length.py
@@ -1,6 +1,8 @@
 country1 = "Bangladesh is a beautiful country"
+country2 = "India is not a beautiful country"
 length = len(country1)
 
-#print (length)
+print (length)
 
 print (f" Length of the Bangladesh country charachter is: {len(country1)}")
+print (country2)
